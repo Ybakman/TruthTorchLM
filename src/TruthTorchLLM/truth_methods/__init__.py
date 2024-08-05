198429138983527501d4112f9e5509fe4c1bf993
hayrettinerenyildiz <hayereyil@gmail.com>
2024-08-05 16:24:41 +0300

    blackbox methods are added

--- src/TruthTorchLLM/truth_methods/__init__.py
+++ src/TruthTorchLLM/truth_methods/__init__.py
@@ -4,6 +4,7 @@ from .confidence import Confidence
 from .semantic_entropy import SemanticEntropy
 from .google_search_check import GoogleSearchCheck
 from .p_true import PTrue
+from .blackbox import BlackBoxMethods
 
 
-__all__ = ['Entropy', 'Confidence', 'TruthMethod', 'SemanticEntropy', 'PTrue', 'GoogleSearchCheck']
+__all__ = ['Entropy', 'Confidence', 'TruthMethod', 'SemanticEntropy', 'PTrue', 'GoogleSearchCheck','BlackBoxMethods']
--- src/TruthTorchLLM/utils/utils.py
+++ src/TruthTorchLLM/utils/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast
 import re
+from sklearn.metrics import jaccard_score
+from sklearn.feature_extraction.text import CountVectorizer
 
 def sigmoid_normalization(x: float, threshold: float = 0.0, std: float = 1.0):
     return 1 / (1 + np.exp(- (x - threshold) / std))
@@ -32,6 +34,19 @@ def find_token_indices(tokens:list, tokenizer:PreTrainedTokenizer, target_text:s
             found = False
     return indices, texts
 
+
+def calculate_jaccard_similarity(text1: str, text2: str) -> float:
+    vectorizer = CountVectorizer().fit([text1, text2])
+    vectors = vectorizer.transform([text1, text2]).toarray()
+    
+    intersection = (vectors[0] & vectors[1]).sum()
+    union = (vectors[0] | vectors[1]).sum()
+    print(intersection)
+    print(union)
+    return intersection / union if union != 0 else 0
+
+
+
 # Function to check entailment between two sequences
 def check_entailment(model_for_entailment: PreTrainedModel, tokenizer_for_entailment: PreTrainedTokenizer, context: str, seq1: str, seq2: str):
     inputs = tokenizer_for_entailment.encode_plus(
@@ -47,20 +62,28 @@ def check_entailment(model_for_entailment: PreTrainedModel, tokenizer_for_entail
     
     return out_class
 
+
 # Function to perform bidirectional entailment clustering
-def bidirectional_entailment_clustering(model_for_entailment: PreTrainedModel, tokenizer_for_entailment: PreTrainedTokenizer, context : str, sequences: list[str]):
+def bidirectional_entailment_clustering(model_for_entailment: PreTrainedModel, tokenizer_for_entailment: PreTrainedTokenizer, context : str, sequences: list[str],method = "semantic"):
     clusters = [{sequences[0]}]
     for s_m in sequences[1:]:
         added_to_class = False
         for c in clusters:
             s_c = next(iter(c))  # Use the first sequence in the class for comparison
-            left = check_entailment(model_for_entailment, tokenizer_for_entailment, context, s_c, s_m)
-            right = check_entailment(model_for_entailment, tokenizer_for_entailment, context, s_m, s_c)
-            
-            if left != 0 and right != 0:#it shows there is no contradiction
-                c.add(s_m)
-                added_to_class = True
-                break
+            if method == "semantic":
+                left = check_entailment(model_for_entailment, tokenizer_for_entailment, context, s_c, s_m)
+                right = check_entailment(model_for_entailment, tokenizer_for_entailment, context, s_m, s_c)
+                
+                if left != 0 and right != 0:#it shows there is no contradiction
+                    c.add(s_m)
+                    added_to_class = True
+                    break
+            elif method == "jaccard":
+                similarity = calculate_jaccard_similarity(s_c, s_m)
+                if similarity >= 0.7:
+                    c.add(s_m)
+                    added_to_class = True
+                    break
         
         if not added_to_class:
             clusters.append({s_m})
